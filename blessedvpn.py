b0179d58b92681d40350711929c813ab2afd21f4
blessedcodess <taiyr7080@gmail.com>
2026-06-27 19:21:53 +0500

    Update blessedvpn.py
    fixed termux downloading

--- blessedvpn.py
+++ blessedvpn.py
@@ -205,7 +205,7 @@ PACKAGE_MANAGERS = {
     '10': ("nix-env",      "NixOS",                        ['nix-env', '-iA', 'nixpkgs.openvpn']),
     '11': ("brew",         "macOS (Homebrew)",             ['brew', 'install', 'openvpn']),
     '12': ("pkg",          "FreeBSD",                      ['sudo', 'pkg', 'install', '-y', 'openvpn']),
-    '13': ("pkg",          "Termux (Android)",             ['pkg', 'install', '-y', 'openvpn']),
+    '13': ("pkg",          "Termux (Android)",             ['pkg', 'install', '-y', 'openvpn3']),
 }
 
 
